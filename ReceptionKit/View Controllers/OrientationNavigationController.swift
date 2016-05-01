87e46076564c6f6604cd8eecfab685e9426f1867
Andy Cho <acromace@gmail.com>
2016-05-01 03:15:07 -0700

    Add orientation based constraints

---
+++ ReceptionKit/View Controllers/OrientationNavigationController.swift
@@ -0,0 +1,22 @@
+//
+//  OrientationNavigationController.swift
+//  ReceptionKit
+//
+//  Created by Andy Cho on 2016-05-01.
+//  Copyright © 2016 Andy Cho. All rights reserved.
+//
+
+import UIKit
+
+class OrientationNavigationController: UINavigationController {
+
+    // Ensure that the orientation change triggers an Auto Layout change on the iPad
+    // Otherwise, the iPad dimensions are always Regular x Regular
+    override func overrideTraitCollectionForChildViewController(childViewController: UIViewController) -> UITraitCollection? {
+        if view.bounds.width < view.bounds.height {
+            return UITraitCollection(horizontalSizeClass: .Compact)
+        }
+        return UITraitCollection(horizontalSizeClass: .Regular)
+    }
+
+}
